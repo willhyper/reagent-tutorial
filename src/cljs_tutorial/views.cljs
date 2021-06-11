9155d499f55e5251754458392643c83109d98740
Chao-Wei Chen <willhyper@gmail.com>
2021-06-11 07:59:30 -0700

    +(defn mirror-text []

--- src/cljs_tutorial/views.cljs
+++ src/cljs_tutorial/views.cljs
@@ -12,10 +12,20 @@
             :value "inc"
             :on-click #(re-frame/dispatch [:cnt-event inc])}]])
 
+(def text-val (atom ""))
+
+(defn mirror-text []
+  [:div @text-val
+   [:input {:type "text"
+                 :value @text-val
+                 :on-change #(reset! text-val (-> % .-target .-value))}]]
+  )
+
 (defn main-panel []
   (let [name (re-frame/subscribe [::subs/name])]
     [:div
      [:h1
       "Hello from " @name]
      [counter]
+     [mirror-text]
      ]))
