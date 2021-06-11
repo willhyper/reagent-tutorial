c456f6ba96585e2b434c9ed212d0d5911b328edb
Chao-Wei Chen <willhyper@gmail.com>
2021-06-11 04:11:04 -0700

    reagent counter

--- src/cljs_tutorial/views.cljs
+++ src/cljs_tutorial/views.cljs
@@ -1,12 +1,22 @@
 (ns cljs-tutorial.views
   (:require
    [re-frame.core :as re-frame]
+   [reagent.core :refer [atom]]
    [cljs-tutorial.subs :as subs]
    ))
+
+(def cnt (atom 0))
+
+(defn counter []
+  [:div [:h1 ">>> " @cnt " "]
+   [:input {:type "button"
+            :value "inc"
+            :on-click #(swap! cnt inc)}]])
 
 (defn main-panel []
   (let [name (re-frame/subscribe [::subs/name])]
     [:div
      [:h1
       "Hello from " @name]
+     [counter]
      ]))
