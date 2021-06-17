981df9d6c8a0041c61fc2839617ea933a5f53df7
Chao-Wei Chen <willhyper@gmail.com>
2021-06-17 04:55:52 -0700

    +(defn slide-color []

--- src/cljs_tutorial/views.cljs
+++ src/cljs_tutorial/views.cljs
@@ -54,6 +54,26 @@
                     (< v 90) ["red"])]
     [:h1 {:style {:color color}} "color"]
     ))
+
+(def rgb (atom {:r 0 :g 0 :b 0}))
+
+(defn slide-color []
+  [:div
+   (for [c  [:r :g :b]]
+     [:input {:type "range" :min 0 :max 255
+              :style {:width "70%"}
+              :value  (c @rgb)
+              :on-change (fn [e] (let [v (.. e -target -value)
+                                       vi (js/parseInt v)]
+                                   (swap! rgb assoc c vi)))}])
+   @rgb
+   [:br]
+   (let [r (:r @rgb) g (:g @rgb) b (:b @rgb)
+         [rh gh bh] (map  #(. % toString 16) [r g b])
+         color (str "#" rh gh bh)]
+     [:h1 {:style {:color color}} color])
+   ]
+  )
 (defn main-panel []
   (let [name (re-frame/subscribe [:name])]
     [:div
@@ -66,4 +86,5 @@
      [uili (range 3)]
      [:a {:href "http://google.com"} "google.com"]
      [condition-color]
+     [slide-color]
      ]))
