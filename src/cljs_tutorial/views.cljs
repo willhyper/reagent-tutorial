463ad0f2acedf0735bab672868ea318645802cde
Chao-Wei Chen <willhyper@gmail.com>
2021-07-04 10:33:54 +0800

    tabIndex case sensitive

--- src/cljs_tutorial/views.cljs
+++ src/cljs_tutorial/views.cljs
@@ -4,9 +4,37 @@
    [cljs-tutorial.subs :as subs]
    ))
 
+(def _canvas (atom {}))
+
+(def cam (atom {:x 10 :y 10 :angle 0}))
+(def mouse (atom {:x 10 :y 10}))
+
+(defn canvas_track_mouse []
+  [:div
+   {:onMouseMove (fn [e]
+                   (let [ctx (.getContext @_canvas "2d")
+                         w (.-width @_canvas) h (.-height @_canvas)
+                         mx (.-clientX e) my (.-clientY e)
+                         cx (:x @cam) cy (:y @cam)
+                         dx (- mx cx) dy (- my cy) ang (Math/atan (/ dy dx))
+                         cxnew (+ cx (/ dx 20)) cynew (+ cy (/ dy 20))]
+                     (swap! mouse assoc :x mx :y my)
+                     (swap! cam assoc :angle ang :x cxnew :y cynew)
+
+                     (set! (. ctx -fillStyle) "black")
+
+                     (.clearRect ctx 0 0 w h)
+                     (.fillRect ctx cx cy 10 10)))}
+   [:canvas {:ref #(reset! _canvas %)
+             :style {:background-color "lightblue"}
+             :tabIndex 1}]
+
+   [:br]
+   cam
+   [:br]
+   mouse
+   ])
+
 (defn main-panel []
-  (let [name (re-frame/subscribe [::subs/name])]
-    [:div
-     [:h1
-      "Hello from " @name]
-     ]))
+  [canvas_track_mouse]
+  )
