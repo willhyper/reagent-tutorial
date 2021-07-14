dabfacb61d5605c32b23cd5ac89fc23b300dceda
Chao-Wei Chen <willhyper@gmail.com>
2021-07-14 15:56:29 +0800

    draw @_canvas_cam in maze

--- src/cljs_tutorial/views.cljs
+++ src/cljs_tutorial/views.cljs
@@ -4,44 +4,37 @@
    [cljs-tutorial.subs :as subs]
    ))
 
-(def _canvas (atom {}))
 
-(def cam (atom {:angle 0}))
+(def deg2rad (/ Math/PI 180))
+(def _canvas (atom {}))
+(def _canvas_cam (atom {}))
+
+
+(def cam (atom {:x 100 :y 100 :angle 0}))
 (def mouse (atom {:x 10 :y 10}))
 (def fov (atom {}))
-(def debug (atom {}))
-
-(defn wall-height [H angle]
-  (->> angle (* (/ Math/PI 180)) Math/atan (* (/ 2 Math/PI))  (- 1) (* (/ H 2))))
-
-(defn wall-coordinate [H W angle]
-  (let [wh (wall-height H angle)]
-    [(+ (/ W 2) (* angle (/ W 120))) 
-     (- (/ H 2) (/ wh 2)) 
-     15
-     wh]))
+(def maze (atom {}))
 
 (defn canvas_track_mouse []
   [:div
    {:onMouseMove (fn [e]
                    (let [ctx (.getContext @_canvas "2d")
                          w (.-width @_canvas) h (.-height @_canvas)
+                         wmaze (.-width @_canvas_cam) hmaze (.-height @_canvas_cam)
                          {top :top left :left right :right bottom :bottom centerx :centerx centery :centery} @fov ; absolute fov coordinate 
                          mx (.-clientX e) my (.-clientY e) ; absolute mouse x y coordinate
                          mox (- mx left) moy (- my top) ; mouse x y relative to fov origin
                          mcx (- mox centerx) mcy (- moy centery) ; mouse x y relative to fov center in pixel
 
-                         ; rule of assigning new camera x y
-                        ;;  cx (:x @cam) cy (:y @cam) ; camera x y relative to fov origin
-                        ;;  dcx (- mox cx) dcy (- moy cy) ; mouse camera x y difference
-                        ;;  cxnew (int (+ cx (/ dcx 5)))  cynew (int (+ cy (/ dcy 5))) 
-
                          cangle (:angle @cam) ; in degree
                          angle (* 60 (/ mcx w)) ; range of (/mcx w) is (-0.5 0.5). *60 gives (-30 30) as viewing angle range in degree
-                         canglenew  (+ cangle (/ (- angle cangle) 10)) ; rule of assigning new camera angle: in 10 steps cangle to reach angle
+
+                         canglenew  (+ cangle (/ angle 10)) ; rule of assigning new camera angle: in 10 steps cangle to reach angle
+                         cxnew (min wmaze (max 0 (+ (:x @cam) (Math/cos (* canglenew deg2rad)))))
+                         cynew (min hmaze (max 0 (+ (:y @cam) (Math/sin (* canglenew deg2rad)))))
                          ] 
                      (swap! mouse assoc :x mcx :y mcy)
-                     (swap! cam assoc :angle canglenew)
+                     (swap! cam assoc :x cxnew :y cynew :angle canglenew)
 
                      (set! (. ctx -fillStyle) "black")
 
@@ -51,7 +44,21 @@
                        (.beginPath) (.moveTo centerx centery) (.lineTo mox moy) (.stroke))
                      
                      
-                     ))}
+                     )
+                     (let [ctx (.getContext @_canvas_cam "2d")
+                           w (.-width @_canvas_cam) h (.-height @_canvas_cam)
+                           {x :x y :y cangle :angle} @cam
+                           R 100
+                           rayx (* R (Math/cos (* deg2rad cangle)))
+                           rayy (* R (Math/sin (* deg2rad cangle)))]
+
+                       (set! (. ctx -fillStyle) "black")
+
+                       (.clearRect ctx 0 0 w h)
+                       (.fillRect ctx (- x 2) (- y 2) 5 5)
+                       (doto ctx
+                         (.beginPath) (.moveTo x y) (.lineTo (+ x rayx) (+ y rayy)) (.stroke)))
+                     )}
    [:canvas {:ref (fn [c]
                     (reset! _canvas c)
 
@@ -67,7 +74,21 @@
              :style {:background-color "lightblue"
                      :width 1200 :height 600}
              :tabIndex 1}]
+   [:canvas {:ref (fn [c]
+                    (reset! _canvas_cam c)
 
+                    ; Caveats with callback refs: you may get nil
+                    ; https://reactjs.org/docs/refs-and-the-dom.html
+                    (if (nil? c)
+                      (); do nothing
+                      (let [rect (.getBoundingClientRect c)
+                            t (.-top rect) r (.-right rect)
+                            b (.-bottom rect) l (.-left rect)]
+                        (swap! maze assoc :top t :left l :right r :bottom b))))
+             :width 400 :height 200 ; https://stackoverflow.com/questions/4938346/canvas-width-and-height-in-html5
+             :style {:background-color "yellow"
+                     :width 400 :height 200}
+             :tabIndex 2}]
    [:br]
    "mouse " mouse
    [:br]
